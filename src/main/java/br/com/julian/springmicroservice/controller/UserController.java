dc9e88057a5de326a9441ee23c594f66e1362cf6
Julian Luís <julian.lfs@gmail.com>
2019-04-28 20:27:37 -0300

    hateoas

--- src/main/java/br/com/julian/springmicroservice/controller/UserController.java
+++ src/main/java/br/com/julian/springmicroservice/controller/UserController.java
@@ -3,6 +3,8 @@ package br.com.julian.springmicroservice.controller;
 import br.com.julian.springmicroservice.model.User;
 import br.com.julian.springmicroservice.service.UserDaoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -10,6 +12,9 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 public class UserController {
@@ -37,8 +42,15 @@ public class UserController {
     }
 
     @GetMapping("users/{id}")
-    public User findOne(@PathVariable("id") Long id) {
-        return userDaoService.findById(id);
+    public EntityModel<User> findOne(@PathVariable("id") Long id) {
+
+        User user = userDaoService.findById(id);
+
+        EntityModel<User> model = new EntityModel<User>(user);
+        WebMvcLinkBuilder linkTo = linkTo(methodOn(this.getClass()).getAll());
+        model.add(linkTo.withRel("all-users"));
+
+        return model;
     }
 
     @DeleteMapping("/users/{id}")
