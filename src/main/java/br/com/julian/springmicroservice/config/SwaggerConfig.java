8f15992f37874e3ffab3537fab0679b24344c322
Julian Luís <julian.lfs@gmail.com>
2019-04-30 23:57:37 -0300

    actuator

---
+++ src/main/java/br/com/julian/springmicroservice/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package br.com.julian.springmicroservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+//@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+
+        return new Docket(DocumentationType.SPRING_WEB);
+    }
+
+
+}
