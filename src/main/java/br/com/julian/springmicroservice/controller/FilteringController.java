74f526126fa1f2dbebef0ec92a683d60c2b7783c
Julian Luís <julian.lfs@gmail.com>
2019-07-09 20:09:59 -0300

    jsonIgnoreProperties

---
+++ src/main/java/br/com/julian/springmicroservice/controller/FilteringController.java
@@ -0,0 +1,17 @@
+package br.com.julian.springmicroservice.controller;
+
+import br.com.julian.springmicroservice.model.SomeBean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FilteringController {
+
+    @GetMapping("/filtering")
+    public SomeBean retrieveSomeBean() {
+
+        return new SomeBean("value1", "value2", "value3");
+
+    }
+
+}
